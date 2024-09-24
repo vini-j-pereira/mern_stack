d72b093df880ad276dd9d0557ed4c11f10d9f473
vini-j-pereira <viniciusjosepereira@gmail.com>
2024-09-24 15:43:35 -0300

    feat: criação de validações pre middleware

--- src/controllers/news.controller.js
+++ src/controllers/news.controller.js
@@ -3,6 +3,25 @@ import { createService, findAllService } from "../services/news.service.js";
 const create = async (req, res) => {
 
     try {
+        const { authorization } = req.headers;
+        
+        if (!authorization) {
+            return res.send(401);
+        }
+
+        const parts = authorization.split(" ");
+
+        if(parts.length !== 2){
+            return res.status(401);
+        }
+        
+        const [schema, token] = parts 
+        
+
+        if(schema !== "Bearer") {
+            return res.send(401);
+        }
+
         const { title, text, banner } = req.body;
 
         if(!title || !text || !banner) {
